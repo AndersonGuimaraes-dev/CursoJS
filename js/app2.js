30ab4dd4ed35142437c6bcdd48da46b4fb41a703
Anderson de Matos Guimarães <andersonmguimaraes@icloud.com>
2021-09-18 09:06:49 -0300

    Document
    USando o comando document.getElementById para mostrar como mostrar as informações (da DIV) e como mostrar apenas o conteúdo da DIV. Usou-se o console.log para mostrar a informação no console

---
+++ js/app2.js
@@ -0,0 +1,17 @@
+alert("Olá, mundo!")
+
+console.log("Eu sou o console")// console.log envia para o console
+
+document.getElementById("titulo")//comando que serve para pegar um elemento, nesse caso pelo ID
+
+console.log(
+    document.getElementById("titulo") 
+    //coloca a DIV no console
+)
+console.log(
+    document.getElementById("titulo").innerHTML
+    // .innerHTML coloca apenas o conteúdo da div, lembrando que o inner faz parte do document.getElementById
+) 
+console.log(
+    screen.width
+)
